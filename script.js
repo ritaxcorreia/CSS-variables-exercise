f1fa1b8fb2ad73a076f30303e2dd3b50e339936b
Rita <rita@ritacorreia.co>
2020-10-09 15:38:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
-    console.log(suffix);
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
